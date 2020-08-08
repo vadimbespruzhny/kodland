d9a1e9915f127e2463fc7f41737ca47b637c4aa3
Vadim <vadik654321@gmail.com>
2020-08-08 19:56:48 +0300

    prepare to deploy

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-$5s58%xh2(3ad97lc)5&r*u1&)2iym!3qsbdao7og^rgsttox'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['radiant-earth-32704.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['stark-plains-14352.herokuapp.com', '127.0.0.1']
 
 # Application definition
 

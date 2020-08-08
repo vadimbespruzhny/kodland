37712417ba7c26def84b2c40ea246632a49c48e1
Vadim <vadik654321@gmail.com>
2020-08-08 20:22:09 +0300

    prepare to deploy

--- my_blog/views.py
+++ my_blog/views.py
@@ -30,10 +30,10 @@ class ArticleListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         queryset = Blog.objects.all()
+        obj = Blog.objects.all()[0]
         paginator = paginate(self.request, queryset, 10)
         context['paginator'] = paginator
-        for i in queryset:
-            context['obj'] = i
+        context['obj'] = obj
         return context
 
 
